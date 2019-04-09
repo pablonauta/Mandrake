cb89bfe66a8a481f514da3f6b2367d0d1facc52b
Pablonauta <pablonauta@gmail.com>
2019-04-09 00:27:45 -0300

    comit 2

--- Cppes/Gato.cpp
+++ Cppes/Gato.cpp
@@ -1,8 +1,5 @@
 #include "../Clases/Gato.h"
 
-Gato::Gato(const DtGato& bicho) :  DtGato(bicho.getNombre(), bicho.getGenero(), bicho.getPeso()){
-	this->pelo = bicho.TipoPelo;
-}
 
 Gato::~Gato(){
 
